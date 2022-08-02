0e87fe7050a1c6a48d8700f8c0164ee9d1b48a8e
cirilobido <ciriloenmanuel@gmail.com>
2022-08-02 19:25:22 -0400

    :construction: View Model and State Added

---
+++ app/src/main/java/com/androidstudy/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.androidstudy.weatherapp.presentation
+
+import com.androidstudy.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
---
+++ app/src/main/java/com/androidstudy/weatherapp/presentation/WeatherViewModel.kt
@@ -0,0 +1,54 @@
+package com.androidstudy.weatherapp.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androidstudy.weatherapp.domain.WeatherRepository
+import com.androidstudy.weatherapp.domain.location.LocationTracker
+import com.plcoding.weatherapp.domain.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val repository: WeatherRepository,
+    private val locationTracker: LocationTracker
+) : ViewModel() {
+    var state by mutableStateOf(WeatherState())
+        private set
+
+    fun loadWeatherInfo() {
+        viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+                error = null
+            )
+            locationTracker.getCurrentLocation()?.let { location ->
+                when (val result =
+                    repository.getWeatherData(location.longitude, location.longitude)) {
+                    is Resource.Success -> {
+                        state = state.copy(
+                            isLoading = false,
+                            weatherInfo = result.data
+                        )
+                    }
+                    is Resource.Error -> {
+                        state = state.copy(
+                            isLoading = false,
+                            weatherInfo = null,
+                            error = result.message
+                        )
+                    }
+                }
+            } ?: kotlin.run {
+                state = state.copy(
+                    isLoading = false,
+                    error = "Couldn't retrieve location."
+                )
+            }
+        }
+    }
+}
